1e996895204ffb2d4ae4c91ea8b8f354b5cb447c
Manish Mahato <manishresearch18@gmail.com>
2025-10-18 17:35:30 +0530

    Added string token support in lexer

--- Compiler/Lexer.py
+++ Compiler/Lexer.py
@@ -1,36 +1,37 @@
-import re 
+import re
 
-TOKENS=[
-    ('INT',r'integer'),
-    ('PRINT',r'print'),
-    ('NUMBER',r'\d+'),
-    ('ID',r'[a-zA_Z_][a-zA_Z0-9_]'),
-    ('ASSIGN',r'='),
-    ('PLUS',r'\+'),
-    ('MINUS',r'-'),
-    ('MUL',r'\*'),
-    ('DIV',r'/'),
-    ('LPAREN',r'\('),
-    ('RPAREN',r'\)'),
-    ('SEMICOLON',r';'),
-    ('SKIP',r'[\t\n]+')
+TOKENS = [
+    ('INT', r'integer'),
+    ('PRINT', r'print'),
+    ('STRING', r'"[^"]*"'),        # String literals
+    ('NUMBER', r'\d+'),
+    ('ID', r'[a-zA-Z_][a-zA-Z0-9_]*'),
+    ('ASSIGN', r'='),
+    ('PLUS', r'\+'),
+    ('MINUS', r'-'),
+    ('MUL', r'\*'),
+    ('DIV', r'/'),
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+    ('SEMICOLON', r';'),
+    ('SKIP', r'[\t\n ]+')
 ]
 
 def lexer(code):
-    pos=0
-    tokens=[]
+    pos = 0
+    tokens = []
     while pos < len(code):
-        match=None
-        for token_type,pattern in TOKENS:
-            regex=re.compile(pattern)
-            match=regex.match(code[pos:])
+        match = None
+        for token_type, pattern in TOKENS:
+            regex = re.compile(pattern)
+            match = regex.match(code[pos:])
             if match:
-                text= match.group()
+                text = match.group()
                 if token_type != 'SKIP':
-                    tokens.append((token_type,text))
-                pos+=len(text)
+                    tokens.append((token_type, text))
+                pos += len(text)
                 break
         if not match:
             raise SyntaxError(f'Unexpected charecter : {code[pos]}')
-    tokens.append(('EOF',''))
+    tokens.append(('EOF', ''))
     return tokens
