5dd83cfcccd995c016c96783d78ffef9562bad69
Manish Mahato <manishresearch18@gmail.com>
2025-10-18 17:34:26 +0530

    Refactor PRINT handling and added STRING support
    Updated the PRINT node to evaluate the expression before printing and added support for STRING nodes.

--- Compiler/Evaluator.py
+++ Compiler/Evaluator.py
@@ -6,28 +6,36 @@ class Interpreter:
         if node.type == 'PROGRAM':
             for stmt in node.statements:
                 self.eval(stmt)
+
         elif node.type == 'DECL':
             value = self.eval(node.expr)
             self.symbol_table[node.var] = value
+
         elif node.type == 'ASSIGN':
             value = self.eval(node.expr)
             if node.var not in self.symbol_table:
                 raise NameError(f"Variable '{node.var}' not declared")
             self.symbol_table[node.var] = value
+
         elif node.type == 'PRINT':
-            if node.var not in self.symbol_table:
-                raise NameError(f"Variable '{node.var}' not declared")
-            print(self.symbol_table[node.var])
+            value = self.eval(node.expr)
+            print(value)
+
         elif node.type == 'BINOP':
             left = self.eval(node.left)
             right = self.eval(node.right)
             if node.op == '+': return left + right
             if node.op == '-': return left - right
             if node.op == '*': return left * right
-            if node.op == '/': return left // right  # integer division
+            if node.op == '/': return left // right
+
         elif node.type == 'NUMBER':
             return node.value
+
         elif node.type == 'ID':
             if node.name not in self.symbol_table:
                 raise NameError(f"Variable '{node.name}' not declared")
             return self.symbol_table[node.name]
+
+        elif node.type == 'STRING':
+            return node.value
